d79fcc1d1e55dc50449087a67d1bb697b21ad19c
sirluk <lukas.hauzenberger@gmail.com>
2022-08-29 19:36:52 +0200

    update wrapper

--- main_wrapper.py
+++ main_wrapper.py
@@ -19,13 +19,7 @@ subprocess.call('python3 main.py --modular --gpu_id=3 --seed=0 --modular_adv_tas
 
 subprocess.call('python3 main.py --modular --gpu_id=3 --seed=0 --modular_adv_task_head=False --fixmask_pct=0.02 --model_name="bert-base-uncased" --ds="pan16"' + suffix, shell=True)
 
-subprocess.call('python3 main.py --modular --gpu_id=3 --seed=0 --modular_adv_task_head=True --fixmask_pct=0.01 --model_name="bert-base-uncased" --ds="pan16"' + suffix, shell=True)
-
-subprocess.call('python3 main.py --modular --gpu_id=3 --seed=0 --modular_adv_task_head=True --fixmask_pct=0.02 --model_name="bert-base-uncased" --ds="pan16"' + suffix, shell=True)
-
 subprocess.call('python3 main.py --baseline --modular --gpu_id=3 --seed=0 --modular_adv_task_head=False --model_name="bert-base-uncased" --ds="pan16"' + suffix, shell=True)
-
-subprocess.call('python3 main.py --baseline --modular --gpu_id=3 --seed=0 --modular_adv_task_head=True --model_name="bert-base-uncased" --ds="pan16"' + suffix, shell=True)
 
 subprocess.call('python3 main.py --adv --gpu_id=3 --seed=0 --fixmask_pct=0.01 --model_name="bert-base-uncased" --ds="pan16"' + suffix, shell=True)
 
@@ -35,12 +29,17 @@ subprocess.call('python3 main.py --adv --gpu_id=3 --seed=0 --fixmask_pct=0.02 --
 
 subprocess.call('python3 main.py --gpu_id=3 --seed=0 --fixmask_pct=0.02 --model_name="bert-base-uncased" --ds="pan16"' + suffix, shell=True)
 
+subprocess.call('python3 main.py --baseline --gpu_id=3 --seed=0 --model_name="bert-base-uncased" --ds="pan16"' + suffix, shell=True)
+
+subprocess.call('python3 main.py --baseline --adv --gpu_id=3 --seed=0 --model_name="bert-base-uncased" --ds="pan16"' + suffix, shell=True)
 
 # BIOS
 
-subprocess.call('python3 main.py --baseline --modular --gpu_id=3 --seed=0 --modular_adv_task_head=False --model_name="bert-base-uncased" --ds="bios"' + suffix, shell=True)
+subprocess.call('python3 main.py --modular --gpu_id=3 --seed=0 --modular_adv_task_head=False --fixmask_pct=0.01 --model_name="bert-base-uncased" --ds="bios"' + suffix, shell=True)
 
-subprocess.call('python3 main.py --baseline --modular --gpu_id=3 --seed=0 --modular_adv_task_head=True --model_name="bert-base-uncased" --ds="bios"' + suffix, shell=True)
+subprocess.call('python3 main.py --modular --gpu_id=3 --seed=0 --modular_adv_task_head=False --fixmask_pct=0.02 --model_name="bert-base-uncased" --ds="bios"' + suffix, shell=True)
+
+subprocess.call('python3 main.py --baseline --modular --gpu_id=3 --seed=0 --modular_adv_task_head=False --model_name="bert-base-uncased" --ds="bios"' + suffix, shell=True)
 
 subprocess.call('python3 main.py --adv --gpu_id=3 --seed=0 --fixmask_pct=0.01 --model_name="bert-base-uncased" --ds="bios"' + suffix, shell=True)
 
