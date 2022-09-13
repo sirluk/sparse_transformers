13013c76055c27acdc74d887dba9fa7eb64c6867
sirluk <lukas.hauzenberger@gmail.com>
2022-09-13 10:39:18 +0200

    move old code

--- src/models/weight_parametrizations.py
+++ src/models/weight_parametrizations.py
@@ -4,14 +4,19 @@ import torch
 from  torch import nn
 from torch.nn.parameter import Parameter
 
-from typing import Tuple
-
 from src.utils import concrete_stretched
 
 
 class DiffWeightFinetune(nn.Module):
 
-    def __init__(self, weight, alpha_init, concrete_lower, concrete_upper, structured):
+    def __init__(
+        self,
+        weight: nn.Parameter,
+        alpha_init: float,
+        concrete_lower: float,
+        concrete_upper: float,
+        structured: bool
+    ):
         super().__init__()
         self.concrete_lower = concrete_lower
         self.concrete_upper = concrete_upper
@@ -80,57 +85,3 @@ class DiffWeightFixmask(nn.Module):
 
     def set_frozen(self, frozen: bool) -> None:
         self.diff_weight.requires_grad = not frozen
-
-
-class DoubleDiffWeightFinetune(nn.Module):
-
-    class WeightState(Enum):
-        TRAIN_FIRST = auto()
-        TRAIN_SECOND = auto()
-
-    def __init__(self, *args, **kwargs):
-        super().__init__()
-        self.diff_weight_1 = DiffWeightFinetune(*args, **kwargs)
-        self.diff_weight_2 = DiffWeightFinetune(*args, **kwargs)
-        self.register_parameter("testpar", Parameter(torch.zeros((1,), device=args[0].device)))
-        self.set_weight_state()
-
-    def forward(self, X):
-        if self.weight_state == self.WeightState.TRAIN_FIRST:
-            return self.diff_weight_1(X)
-        else:
-            with torch.no_grad(), self.deterministic():
-                diff_first = self.diff_weight_1.diff_weight(X)
-                temp_second = self.diff_weight_2.finetune - X - diff_first
-            diff_second = self.diff_weight_2.diff_weight((X+diff_first))
-            return (self.diff_weight_2.finetune - temp_second) + self.diff_weight_1.z * diff_second
-
-    @torch.no_grad()
-    def get_diff_weights(self, X: Parameter) -> Tuple[torch.Tensor, torch.Tensor]:
-        with self.deterministic():
-            diff_first = self.diff_weight_1.diff_weight(X)
-            diff_second = self.diff_weight_1.diff_weight((X + diff_first))
-            return diff_first, self.diff_weight_1.z * diff_second
-
-    @torch.no_grad()
-    def get_z_masks(self) -> Tuple[torch.Tensor, torch.Tensor]:
-        with self.deterministic():
-            return self.diff_weight_1.z, self.diff_weight_1.z * self.diff_weight_2.z
-
-    def set_weight_state(self, first: bool = True) -> None:
-        if first:
-            self.diff_weight_1.set_frozen(False)
-            self.diff_weight_2.set_frozen(True)
-            self.weight_state = self.WeightState.TRAIN_FIRST
-        else:
-            self.diff_weight_1.set_frozen(False)
-            self.diff_weight_1.finetune.requires_grad = False
-            self.diff_weight_2.set_frozen(False)
-            self.weight_state = self.WeightState.TRAIN_SECOND
-
-    @contextlib.contextmanager
-    def deterministic(self):
-        tmp_state = self.training
-        if tmp_state: self.eval()
-        yield
-        if tmp_state: self.train()
