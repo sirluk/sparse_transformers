5751ae2af20a291d692b22d82f5cb34e1bca6884
sirluk <lukas.hauzenberger@gmail.com>
2022-10-21 18:36:35 +0200

    script adjustment

--- scripts/merge_models.py
+++ scripts/merge_models.py
@@ -1,3 +1,4 @@
+from logging import LogRecord
 import sys
 sys.path.insert(0,'..')
 
@@ -12,58 +13,70 @@ from src.models.model_task import TaskModel
 from src.models.model_diff_modular import ModularDiffModel
 from src.models.model_heads import ClfHead
 from src.models.model_base import BaseModel
-from src.model_functions import train_head, merge_adv_models, merge_modular_model
+from src.model_functions import train_head, merge_adv_models, merge_modular_model, merge_models
 from src.adv_attack import adv_attack
 from src.data_handler import get_data
 from src.model_functions import generate_embeddings
-from src.utils import get_logger_custom, get_callables, get_param_from_name
+from src.utils import get_logger_custom, get_callables
 
-DEBUG = False
+
+def merge_adv_models_wrapper(cp_gender, cp_age, cp_base = None, mean_diff_weights = True, mean_ignore_zero = False):
+    model_gender = AdvDiffModel.load_checkpoint(cp_gender)
+    model_age = AdvDiffModel.load_checkpoint(cp_age)
+    model = merge_adv_models(
+        model_gender,
+        model_age,
+        mean_diff_weights = mean_diff_weights,
+        mean_ignore_zero = mean_ignore_zero
+    )
+    if cp_base is not None:
+        model_task = TaskModel.load_checkpoint(cp_base)
+        model = merge_models(model_task.encoder, model.encoder, mean = False)
+    return BaseModel(model.model_name, model.state_dict())
+
+
+def merge_modular_models_wrapper(cp_modular, mean_diff_weights = True, mean_ignore_zero = False):
+    modular_model = ModularDiffModel.load_checkpoint(cp_modular)
+    model = merge_modular_model(modular_model, mean_diff_weights = mean_diff_weights, mean_ignore_zero = mean_ignore_zero)
+    return BaseModel(modular_model.model_name, model.state_dict())
+
+
+
+DEBUG = True
 GPU_ID = 0
 SEED = 0
-DEVICE = f"cuda:{GPU_ID}" if torch.cuda.is_available() else "cpu"
 DS = "pan16"
-LOG_DIR = f"logs_merged_masks_{DS}"
-# LOGGER_NAME = f"mod_no_adv_frozen_0.05_seed{SEED}"
-# LOGGER_NAME = f"mod_adv_0.05_seed{SEED}"
-LOGGER_NAME = f"mod_no_adv_0.05_seed{SEED}"
+PCT = 0.1
+MEAN = True
+MEAN_IGNORE_ZERO = False
+
 # CP = {
 #     "task_model": f"/share/home/lukash/pan16/bertl4/cp/task-baseline-bert_uncased_L-4_H-256_A-4-64-2e-05-weighted_loss_prot-seed{SEED}.pt",
-#     "adv_gender": f"/share/home/lukash/pan16/bertl4/cp_cp_init/adverserial-diff_pruning_0.1-bert_uncased_L-4_H-256_A-4-64-2e-05-cp_init-weighted_loss_prot-gender-seed{SEED}.pt",
-#     "adv_age": f"/share/home/lukash/pan16/bertl4/cp_cp_init/adverserial-diff_pruning_0.1-bert_uncased_L-4_H-256_A-4-64-2e-05-cp_init-weighted_loss_prot-age-seed{SEED}.pt"
+#     "adv_gender": f"/share/home/lukash/pan16/bertl4/cp_cp_init/adverserial-diff_pruning_{PCT}-bert_uncased_L-4_H-256_A-4-64-2e-05-cp_init-weighted_loss_prot-gender-seed{SEED}.pt",
+#     "adv_age": f"/share/home/lukash/pan16/bertl4/cp_cp_init/adverserial-diff_pruning_{PCT}-bert_uncased_L-4_H-256_A-4-64-2e-05-cp_init-weighted_loss_prot-age-seed{SEED}.pt"
 # }
 CP = {
-    # "modular_model": f"/share/home/lukash/pan16/bertl4/cp_modular/modular-diff_pruning_0.05-freeze_task_head-bert_uncased_L-4_H-256_A-4-64-2e-05-weighted_loss_prot-gender_age-seed{SEED}.pt"
-    # "modular_model": f"/share/home/lukash/pan16/bertl4/cp_modular/modular-diff_pruning_0.05-adv_task_head-bert_uncased_L-4_H-256_A-4-64-2e-05-weighted_loss_prot-gender_age-seed{SEED}.pt"
-    "modular_model": f"/share/home/lukash/pan16/bertl4/cp_modular/modular-diff_pruning_0.05-bert_uncased_L-4_H-256_A-4-64-2e-05-weighted_loss_prot-gender_age-seed{SEED}.pt"
-
+    "modular_model": f"/share/home/lukash/pan16/bertl4/cp_modular/modular-diff_pruning_{PCT}-freeze_task_head-bert_uncased_L-4_H-256_A-4-64-2e-05-weighted_loss_prot-gender_age-seed{SEED}.pt"
+    # "modular_model": f"/share/home/lukash/pan16/bertl4/cp_modular/modular-diff_pruning_{PCT}-adv_task_head-bert_uncased_L-4_H-256_A-4-64-2e-05-weighted_loss_prot-gender_age-seed{SEED}.pt"
+    # "modular_model": f"/share/home/lukash/pan16/bertl4/cp_modular/modular-diff_pruning_{PCT}-bert_uncased_L-4_H-256_A-4-64-2e-05-weighted_loss_prot-gender_age-seed{SEED}.pt"
 }
 
+LOG_DIR = f"logs_merged_masks_{DS}"
 
-def merge_adv_models_wrapper(cp_gender, cp_age, cp_base = None):
-
-    if cp_base is None:
-        model_gender = AdvDiffModel.load_checkpoint(cp_gender)
-        model_age = AdvDiffModel.load_checkpoint(cp_age)
-        model = merge_adv_models(model_gender, model_age)
-        return BaseModel(model_gender.model_name, model.state_dict())
-    else:
-        model_gender = AdvDiffModel.load_checkpoint(cp_gender, remove_parametrizations=True)
-        model_age = AdvDiffModel.load_checkpoint(cp_age, remove_parametrizations=True)
-        model_task = TaskModel.load_checkpoint(cp_base)
-        base_weights = model_task.encoder
-        with torch.no_grad():
-            for n, p in base_weights.named_parameters():
-                p_gender = get_param_from_name(model_gender.encoder, n)
-                p_age = get_param_from_name(model_age.encoder, n)
-                p = p_gender + p_age - p
-        return BaseModel(model_gender.model_name, base_weights.state_dict())
+LOGGER_NAME = [
+    "DEBUG" if DEBUG else None,
+    "mod" if ("modular_model" in CP.keys()) else "adv",
+    "adv_task_head" if ("modular_model" in CP.keys()) and ("adv_task_head" in CP["modular_model"]) else None,
+    "frozen_task_head" if ("modular_model" in CP.keys()) and ("freeze" in CP["modular_model"]) else None,
+    str(PCT),
+    "avg" if MEAN else "additive",
+    "ignore_zero" if MEAN and MEAN_IGNORE_ZERO else None,
+    f"seed{SEED}"
+]
+LOGGER_NAME = "_".join([x for x in LOGGER_NAME if x is not None])
 
 
-def merge_modular_models_wrapper(cp_modular):
-    modular_model = ModularDiffModel.load_checkpoint(cp_modular)
-    model = merge_modular_model(modular_model)
-    return BaseModel(modular_model.model_name, model.state_dict())
+DEVICE = f"cuda:{GPU_ID}" if torch.cuda.is_available() else "cpu"
 
 
 def main():
@@ -71,8 +84,17 @@ def main():
     torch.manual_seed(SEED)
     print(f"torch.manual_seed({SEED})")
 
-    # model = merge_adv_models_wrapper(CP["adv_gender"], CP["adv_age"]) # , cp_base=CP["task_model"]
-    model = merge_modular_models_wrapper(CP["modular_model"]) # , cp_base=CP["task_model"]
+    # model = merge_adv_models_wrapper(
+    #     CP["adv_gender"],
+    #     CP["adv_age"],
+    #     mean_diff_weights=MEAN,
+    #     mean_ignore_zero=MEAN_IGNORE_ZERO
+    # ) # , cp_base=CP["task_model"]
+    model = merge_modular_models_wrapper(
+        CP["modular_model"],
+        mean_diff_weights=MEAN,
+        mean_ignore_zero=MEAN_IGNORE_ZERO
+    ) # , cp_base=CP["task_model"]
 
     # # TEMP - for debugging
     # from src.model_functions import get_param_from_name
@@ -117,11 +139,6 @@ def main():
     val_data = generate_embeddings(model, val_loader, forward_fn = lambda m, x: m._forward(**x))
 
     # Training Loop for task
-    ds_train = TensorDataset(train_data[0], train_data[1])
-    ds_val = TensorDataset(val_data[0], val_data[1])
-    train_loader = DataLoader(ds_train, shuffle=True, batch_size=args_attack.attack_batch_size, drop_last=False)
-    val_loader = DataLoader(ds_val, shuffle=False, batch_size=args_attack.attack_batch_size, drop_last=False)
-
     task_head = ClfHead(
         hid_sizes=[model.in_size_heads]*(args_train.task_n_hidden+1),
         num_labels=num_labels,
@@ -129,10 +146,16 @@ def main():
     )
     task_head.to(DEVICE)
 
+    del model
+
+    ds_train = TensorDataset(train_data[0], train_data[1])
+    ds_val = TensorDataset(val_data[0], val_data[1])
+    train_loader = DataLoader(ds_train, shuffle=True, batch_size=args_attack.attack_batch_size, drop_last=False)
+    val_loader = DataLoader(ds_val, shuffle=False, batch_size=args_attack.attack_batch_size, drop_last=False)
+
     loss_fn, pred_fn, metrics = get_callables(num_labels)
 
     task_head = train_head(
-        trainer = model,
         head = task_head,
         train_loader = train_loader,
         val_loader = val_loader,
@@ -159,7 +182,6 @@ def main():
         loss_fn, pred_fn, metrics = get_callables(num_lbl_prot, prot_w)
 
         adv_attack(
-            trainer = model,
             train_loader = train_loader,
             val_loader = val_loader,
             logger = train_logger,
@@ -174,8 +196,7 @@ def main():
             lr = args_attack.learning_rate,
             cooldown = args_attack.cooldown,
             create_hidden_dataloader = False,
-            batch_size = args_attack.attack_batch_size,
-            label_idx = label_idx,
+            device = DEVICE,
             logger_suffix = f"adv_attack_{prot_k}"
         )
 
